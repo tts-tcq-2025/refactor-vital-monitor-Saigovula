411be509ad9eb5d3852b927749b7851f5fca75b6
Saigovula <129597706+Saigovula@users.noreply.github.com>
2025-08-11 17:49:05 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,6 +1,16 @@
 #ifndef MONITOR_H
 #define MONITOR_H
 
-int isVitalOk(float temperature, float pulseRate, float spo2);
+typedef struct {
+    const char* name;
+    float min;
+    float max;
+    const char* alertMsg;
+} VitalLimit;
+
+extern const VitalLimit vitalLimits[];
+extern const int numVitals;
+
+int isVitalOk(const float* readings);
 
 #endif
