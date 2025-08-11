4ac92129f2d88cb3fa513d8a8970df68bafdb225
Saigovula <129597706+Saigovula@users.noreply.github.com>
2025-08-11 18:02:01 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,7 +1,22 @@
 #include <gtest/gtest.h>
-#include "./monitor.h"
+extern "C" {
+    #include "monitor.h"
+}
 
-TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+TEST(Monitor, AllVitalsInRange) {
+    EXPECT_TRUE(vitalsOk(98.6f, 70.0f, 98.0f));
+}
+
+TEST(Monitor, TemperatureOutOfRange) {
+    EXPECT_FALSE(vitalsOk(103.0f, 70.0f, 98.0f));
+    EXPECT_FALSE(vitalsOk(94.0f, 70.0f, 98.0f));
+}
+
+TEST(Monitor, PulseRateOutOfRange) {
+    EXPECT_FALSE(vitalsOk(98.6f, 50.0f, 98.0f));
+    EXPECT_FALSE(vitalsOk(98.6f, 120.0f, 98.0f));
+}
+
+TEST(Monitor, SpO2OutOfRange) {
+    EXPECT_FALSE(vitalsOk(98.6f, 70.0f, 85.0f));
 }
